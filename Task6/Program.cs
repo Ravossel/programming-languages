cde0ad8193031c09809fd5e93755f7b59d34af0b
Anastasia Velyuga <velnast@gmail.com>
2023-06-14 01:42:25 +0300

    Hurray! It's... - Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "сема")
+{
+    Console.WriteLine("Ура, это же Сема!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
