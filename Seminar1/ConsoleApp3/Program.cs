d1346f2ecd8e1656b273e05f0ee2dd4caf06b187
Anastasia Velyuga <velnast@gmail.com>
2023-06-15 14:33:04 +0300

    Seminar ConsoleApp3

---
+++ Seminar1/ConsoleApp3/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+/*Задача №3 Напишите программу, которая будет выдавать название дня недели по заданному номеру.*/
+
+Console.Write("Введите целое число от 1 до 7: ");
+int number = int.Parse(Console.ReadLine());
+
+switch (number)
+{
+    case (1):
+    Console.WriteLine("Понедельник");
+    break;
+
+    case (2):
+    Console.WriteLine("Вторник");
+    break;
+    
+    case (3):
+    Console.WriteLine("Среда");
+    break;
+
+    case (4):
+    Console.WriteLine("Четверг");
+    break;
+
+    case (5):
+    Console.WriteLine("Пятница");
+    break;
+
+    case (6):
+    Console.WriteLine("Суббота");
+    break;
+
+    case (7):
+    Console.WriteLine("Воскресенье");
+    break;
+
+    default:
+    Console.WriteLine("Неизвестный день недели");
+    break;
+}
