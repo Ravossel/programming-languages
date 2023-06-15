bc90a27d457327c93b9f00d3898136fa6141e082
Anastasia Velyuga <velnast@gmail.com>
2023-06-15 15:48:29 +0300

    Range of even numbers

---
+++ Seminar1/Home_task_5/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите программу, которая на вход принимает число (N),
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите целое положительное число: ");
+        int num = int.Parse(Console.ReadLine());
+        int start = (2);
+
+        for (int i = start; i <= num; i = i + 2)
+        {
+            Console.Write(i + " ");
+        }
+    }
+}
