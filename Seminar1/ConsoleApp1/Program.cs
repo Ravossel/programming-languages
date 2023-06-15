fd7024465ecf9cc6017142ba701e13f44bd68c4c
Anastasia Velyuga <velnast@gmail.com>
2023-06-15 14:32:00 +0300

    Seminar ConsoleApp1

---
+++ Seminar1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+/*Задача №1 Напишите программу, которая на вход принимает число
+и выдает его квадрат (число умноженное на само себя).*/
+
+Console.Write("Введите целое число: ");
+
+int number = int.Parse(Console.ReadLine());
+
+int result = number * number;
+
+Console.Write($"Квадрат входного числа: {result}");
