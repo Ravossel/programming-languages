fe3950a6ac34f10a0029b37ed524006f43a6e3ed
Anastasia Velyuga <velnast@gmail.com>
2023-06-16 00:04:23 +0300

    Add: Random number without second digit

---
+++ Seminar2/Home_task2/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+456 -> 46
+782 -> 72
+918 -> 98*/
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное рандомное число: {number}");
+
+int FirstDigit = number / 100;
+int ThirdDigit = number % 10;
+
+Console.WriteLine($"Число {number} без второго разряда {FirstDigit}{ThirdDigit}");
