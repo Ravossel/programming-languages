fe39b3991627b521d4ce2435e77031d9a973e6c4
Anastasia Velyuga <velnast@gmail.com>
2023-06-15 22:06:12 +0300

    Add: Array index == find

---
+++ Lecture2/Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 12, 23, 34, 45, 56, 67, 78, 89, 90 };
+
+int n = array.Length;
+int find = 56;
+int index = 0;
+
+while (index < n)
+{
+if (array[index] == find)
+{
+    Console.WriteLine(index);
+    break;
+}
+    //index = index + 1;
+    index++;
+}
