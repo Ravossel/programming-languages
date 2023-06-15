944fd78fa24479d744590ffd8ba997f9d7f02f2f
Anastasia Velyuga <velnast@gmail.com>
2023-06-15 14:37:23 +0300

    Range of number from negative to positive

---
+++ Seminar1/Home_task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Напишите программу, которая на вход принимает одно число (N),
+а на выходе показывает все целые числа в промежутке от -N до N.
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+2 -> " -2, -1, 0, 1, 2"
+*/
+
+using System;
+class Program {
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+        int negative_number = number * -1;
+
+        for (int i = negative_number; i <= number; i++) {
+            Console.Write(i + " ");
+        }
+        //Console.ReadKey();
+    }
+}
