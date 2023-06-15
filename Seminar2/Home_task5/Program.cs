fb50e097380bb36331cdfb119680fb62afc817df
Anastasia Velyuga <velnast@gmail.com>
2023-06-16 02:34:47 +0300

    Add: Second digit of triple digits number

---
+++ Seminar2/Home_task5/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+
+int secondDigit;
+
+if (num < 1000 && num > 99)
+{
+    secondDigit = num % 100 / 10;
+Console.Write(secondDigit);
+    
+}
+else
+{
+ Console.Write("Число не трехзначное");
+}
