07f4558814799222e4eb060f59dd95688b8cbd5d
Anastasia Velyuga <velnast@gmail.com>
2023-06-15 14:32:47 +0300

    Seminar ConsoleApp2

---
+++ Seminar1/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача №2. Напишите программу, которая на вход принимает два числа и проверяет,
+является ли первое число квадратом второго.*/
+
+Console.Write("Введите первое целое число: ");
+int number_1 = int.Parse(Console.ReadLine());
+
+
+Console.Write("Введите второе целое число: ");
+int number_2 = int.Parse(Console.ReadLine());
+
+int number_3 = number_1 * number_1;
+
+if(number_3 == number_2)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
