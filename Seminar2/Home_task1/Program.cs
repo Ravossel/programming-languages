2b8c5f51f685a44e57198204e8edbe221adcafef
Anastasia Velyuga <velnast@gmail.com>
2023-06-15 23:50:15 +0300

    Add: Random number

---
+++ Seminar2/Home_task1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая выводит случайное число из отрезка [10, 99]
+и показывает наибольшую цифру числа.
+Например:
+78 -> 8
+12-> 2
+85 -> 8*/
+
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Рандомное число: {number}");
+
+int num1 = number / 10;
+int num2 = number % 10;
+
+if (num1 > num2)
+{
+    Console.WriteLine($"Наибольшее число {num1}");
+}
+else
+{
+    Console.WriteLine($"Наибольшее число {num2}");
+}
