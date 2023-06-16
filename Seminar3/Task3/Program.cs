5d168b9caa87fbd65b36587ae1c0848c6e08583f
Anastasia Velyuga <velnast@gmail.com>
2023-06-17 02:44:16 +0300

    Add: quaters from 1 to N

---
+++ Seminar3/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+от 1 до N.
+
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4*/
+
+void GetSquare(int n)
+{
+    int i = 1;
+    int square;
+    while (i <= n)
+    {
+        square = i * i;
+        Console.Write($"{square}, ");
+        i++;
+    }
+}
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+GetSquare(n);
