5d3d3afc703e667efaf8ca6035ff99a024254257
Anastasia Velyuga <velnast@gmail.com>
2023-06-17 02:08:02 +0300

    Add: Distance between two coordinates in 2D

--- Seminar3/Task1/Program.cs
+++ Seminar3/Task1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая принимает на вход координаты двух
+точек и находит расстояние между ними в 2D пространстве.
+
+A (3,6); B (2,1) -> 5,09 
+A (7,-5); B (1,-1) -> 7,21*/
+
+double GetDistance(int x1, int y1, int x2, int y2)
+{
+    double result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+    return result;
+}
+
+
+Console.Write("Введите x1: ");
+int x1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите y1: ");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите x2: ");
+int x2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите y2: ");
+int y2 = int.Parse(Console.ReadLine());
+
+double finish = GetDistance(x1, y1, x2, y2);
+Console.Write(Math.Round(finish, 2, MidpointRounding.ToNegativeInfinity));
