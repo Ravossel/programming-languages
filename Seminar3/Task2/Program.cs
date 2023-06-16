795abf23f9c7caacc42cd9be603a259105512d56
Anastasia Velyuga <velnast@gmail.com>
2023-06-17 02:11:15 +0300

    Add: range of x and y in specific quarter

--- Seminar3/Task2/Program.cs
+++ Seminar3/Task2/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, которая по заданному номеру четверти, показывает диапазон
+возможных координат точек в этой четверти (x и y).*/
+
+
+string PrintQuarter (int quarter)
+{
+    switch (quarter)
+{
+    case 1:
+    Console.WriteLine("x > 0; y > 0");
+    return "Hello, world";
+    break;
+
+    case 2:
+    Console.WriteLine("x < 0; y > 0");
+    return "Hello, world";
+    break;
+
+    case 3:
+    Console.WriteLine("x < 0; y < 0");
+    return "Hello, world";
+    break;
+
+    case 4:
+    Console.WriteLine("x > 0; y < 0");
+    return "Hello, world";
+    break;
+
+    default:
+    Console.WriteLine("такой нет");
+    return "Hello, world";
+    break;
+
+}
+}
+
+
+Console.Write("Введите четверть: ");
+int quarter = int.Parse(Console.ReadLine());
+
+PrintQuarter(quarter);
+
