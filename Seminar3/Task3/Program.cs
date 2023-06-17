c0576544f4b27c779ffd2776e02d8ac37737d7cb
Anastasia Velyuga <velnast@gmail.com>
2023-06-17 03:18:38 +0300

    Feat: fixed

--- Seminar3/Task3/Program.cs
+++ Seminar3/Task3/Program.cs
@@ -4,19 +4,25 @@
 5 -> 1, 4, 9, 16, 25.
 2 -> 1,4*/
 
-void GetSquare(int n)
+void GetSquare (int n)
 {
-    int i = 1;
     int square;
+    int i = 1;
     while (i <= n)
-    {
-        square = i * i;
-        Console.Write($"{square}, ");
-        i++;
-    }
+{
+    square = i * i;
+    Console.Write($"{square}, ");
+    i++;
 }
+
+}
+
+
 
 Console.Write("Введите число: ");
 int n = int.Parse(Console.ReadLine());
 
 GetSquare(n);
+
+
+
