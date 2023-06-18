4516f77394beef678cbe55802f37dad1e38a0153
Anastasia Velyuga <velnast@gmail.com>
2023-06-18 18:23:09 +0300

    :Add: cube number

---
+++ Seminar3/Home_task3/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 23
+Напишите программу, которая принимает на вход число (N)
+и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+void PrintGrid (int num)
+{
+int cube;
+int i = 1;
+while (i <= num)
+{
+    cube = i * i * i;
+    Console.Write($"{cube}, ");
+    i++; 
+}
+}
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+PrintGrid(num);
