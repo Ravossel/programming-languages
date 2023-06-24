91dd0cce2174375c3400e243ce3261e86b3af416
Anastasia Velyuga <velnast@gmail.com>
2023-06-24 13:47:41 +0300

    Add: Count numbers

---
+++ Seminar4/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse((Console.ReadLine()));
+
+int number_positive = Math.Abs(number);
+int count = 0;
+
+int GetNumbers(int number_positive)
+{
+    if (number_positive == 0)
+    {
+        count = 1;
+    }
+
+    else
+    {
+
+        while (number_positive > 0)
+        {
+            number_positive = number_positive / 10;
+            count = count + 1;
+        }
+    }
+    return count;
+
+}
+
+int result = GetNumbers(number_positive);
+Console.Write($"Количество цифр: {result}");
