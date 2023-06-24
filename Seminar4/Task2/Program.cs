fe0664a61f249c4db0fd321e25cb7598b60a2bb7
Anastasia Velyuga <velnast@gmail.com>
2023-06-24 14:19:38 +0300

    Add: Factorial

---
+++ Seminar4/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int i = 1;
+
+int GetFactorial(int number)
+{
+    while (number > 1)
+    {
+        i = i * number;
+        number = number - 1;
+    }
+    return i;
+}
+
+// Console.WriteLine(i);
+int result = GetFactorial(number);
+Console.WriteLine($"Факториал равен: {result}");
