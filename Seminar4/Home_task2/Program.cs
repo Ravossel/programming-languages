520ed1f5845c4614111f187b5c0f7b9f27c9a16c
Anastasia Velyuga <velnast@gmail.com>
2023-06-26 17:36:42 +0300

    Add: Count numbers

---
+++ Seminar4/Home_task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+
+
+int GetSum(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int inputNumber = int.Parse(Console.ReadLine());
+int number = Math.Abs(inputNumber);
+
+Console.WriteLine($"Сумма цифр: {GetSum(number)}");
