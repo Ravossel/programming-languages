1e5fee3ab2b7d6a89afcdc8dca7f777281f99c93
Anastasia Velyuga <velnast@gmail.com>
2023-06-26 15:23:16 +0300

    Feat: Count numbers

--- Seminar4/Task1/Program.cs
+++ Seminar4/Task1/Program.cs
@@ -4,11 +4,6 @@
 // 89126 -> 5
 
 
-Console.Write("Введите число: ");
-int number = int.Parse((Console.ReadLine()));
-
-int number_positive = Math.Abs(number);
-int count = 0;
 
 int GetNumbers(int number_positive)
 {
@@ -29,6 +24,12 @@ int GetNumbers(int number_positive)
     return count;
 
 }
+
+Console.Write("Введите число: ");
+int number = int.Parse((Console.ReadLine()));
+
+int number_positive = Math.Abs(number);
+int count = 0;
 
 int result = GetNumbers(number_positive);
 Console.Write($"Количество цифр: {result}");
