c298c23d3b5a76f47c4821a2bb25927afc7c4fea
Anastasia Velyuga <velnast@gmail.com>
2023-06-29 16:49:57 +0300

    Add: task2

---
+++ Lecture3/Task2/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача №2 Упорядочить массив
+
+int [] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1,};
+
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write ($"{array [i]} ");
+    }
+    Console.WriteLine();
+}
+
+//На увеличение
+
+// void SelectionSort (int [] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] < array[minPosition]) minPosition = j;
+//         }
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+//     }
+// }
+
+
+//На уменьшение
+
+void SelectionSort (int [] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
