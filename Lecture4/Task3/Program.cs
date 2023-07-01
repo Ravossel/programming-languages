c0b4c7079a4075178f0bcb718eb1ca0d0b04e8dc
Anastasia Velyuga <velnast@gmail.com>
2023-07-01 17:38:13 +0300

    factorial with big numbers

---
+++ Lecture4/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Факториал с большими числами
+
+//Важно сделать double
+double Factorial(int n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 40; i++)
+//40 - по желанию
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
