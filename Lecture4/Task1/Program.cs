5d2789d5915235e5749120b0faee8999854d2cd2
Anastasia Velyuga <velnast@gmail.com>
2023-07-01 17:29:09 +0300

    Add: Двумерные массивы

---
+++ Lecture4/Task1/Program.cs
@@ -0,0 +1,60 @@
+﻿// // Двумерные массивы
+
+
+
+// //Таблицы
+// string[,] table = new string[2, 5];
+// table[1, 2] = "слово";
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int colums = 0; colums < 5; colums++)
+//     {
+//         Console.WriteLine($"{table[rows, colums]}");
+//     }
+// }
+
+
+// //Матрица чисел
+// int[,] matrix = new int[3, 4];
+// for (int i = 0; i < 3; i++)
+// {
+//     for (int j = 0; j < 4; j++)
+//     {
+//         Console.Write($"{matrix[i, j]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+
+// //Заполнение матрицы случайными числами
+// void PrintArray(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             Console.Write(matr[i, j]);
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+// void FillArray(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             matr[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+// }
+
+// int[,] matrix = new int[3, 4];
+// PrintArray(matrix);
+// FillArray(matrix);
+// Console.WriteLine();
+// PrintArray(matrix);
+
+
