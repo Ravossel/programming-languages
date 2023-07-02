0fe02b8556f45f3bfe35cc111b56151ee0ed7c4b
Anastasia Velyuga <velnast@gmail.com>
2023-07-03 01:18:02 +0300

    Add: dec numbers to bin

---
+++ Seminar6/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+
+void PrintBinNumber(int number)
+{
+    string rest = "";
+    for (int i = 0; 0 < number; i++)
+    {
+        rest = (number % 2) + rest;
+        number = number / 2;
+    }
+    Console.Write(rest);
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+PrintBinNumber(number);
