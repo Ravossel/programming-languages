b6ac096d9fedd08d6f4262af82b464512aeb0c7c
Anastasia Velyuga <velnast@gmail.com>
2023-07-11 02:14:23 +0300

    Add: Home task 1

---
+++ Seminar7/Home_task1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n,
+//заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+// Задайте значения m и n
+Console.Write("Введите m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine());
+
+// Создайте двумерный массив размером m×n
+double[,] array = new double[m, n];
+
+// Создайте объект Random для генерации случайных чисел
+Random random = new Random();
+
+// Заполните массив случайными целыми числами
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = Math.Round(random.NextDouble(), 2);
+    }
+}
+
+// Выведите массив на консоль
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
