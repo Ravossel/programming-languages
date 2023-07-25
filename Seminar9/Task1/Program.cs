c1add4988dc1a1cfa2ee26b728940816347b8164
Anastasia Velyuga <velnast@gmail.com>
2023-07-25 23:03:49 +0300

    Rec Factorial

---
+++ Seminar9/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿//Найти факториал с помощью рекурсии
+
+int GetFactorial(uint n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    int factorial = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        factorial = factorial* i;
+    }
+    return factorial;
+}
+
+int GetRecFactorial(int n)
+{
+    if (n==1) return 1;
+    int temp = n * GetRecFactorial(n-1);
+    return temp;
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int factorial = GetRecFactorial(number);
+Console.WriteLine($"Факториал вашего числа: {factorial}");
