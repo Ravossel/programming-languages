d7a5d2bd70183bdb16b890c1020b273147832df1
Anastasia Velyuga <velnast@gmail.com>
2023-07-25 21:58:48 +0300

    Task1

---
+++ Seminar8/Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задайте двумерный массив. Напишите программу,
+//которая поменяет местами первую и последнюю строку массива
+
+int[,] array = new int[4, 4];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(10);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < array.GetLength(1); i++)
+{
+    int temp = array[array.GetLength(0) - 1, i];
+    array[array.GetLength(0) - 1, i] = array[0, i];
+    array[0, i] = temp;
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
