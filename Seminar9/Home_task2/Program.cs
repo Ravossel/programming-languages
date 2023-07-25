d0b522bf995e6e8874f078625bde8d8f40e4eace
Anastasia Velyuga <velnast@gmail.com>
2023-07-25 23:44:32 +0300

    Sum of numbers from M till N

---
+++ Seminar9/Home_task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int GetSum(int m, int n)
+{
+    int sum = 0;
+    int temp = GetSum(m + 1, n);
+    sum = m + temp;
+    return sum;
+}
+
+
+Console.Write("Введите первое число: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int n = int.Parse(Console.ReadLine());
+
+int result = GetSum(m, n);
+Console.WriteLine(result);
+
